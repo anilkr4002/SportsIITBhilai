d689713437d26a11cc0e3606d3be02aec39e4e5a
anilkrmandal <anilkumarm@iitbhilai.ac.in>
2025-09-03 20:32:06 +0530

    backend url in cors

--- backend/app.py
+++ backend/app.py
@@ -417,6 +417,7 @@ def get_audit_log():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
 else:
     init_db()
