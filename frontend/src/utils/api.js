01b7e1cb289b0fcfb7f78065e2cfff1607ec7b83
anilkrmandal <anilkumarm@iitbhilai.ac.in>
2025-09-03 19:44:59 +0530

    added backend url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
-const API_BASE = 'http://localhost:5000/api';
-
+// const API_BASE = 'http://localhost:5000/api';
+const API_BASE = process.env.REACT_APP_API_URL + "/api";
 // Utility function for API calls
 export const apiCall = async (endpoint, options = {}) => {
   try {
